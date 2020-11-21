8ca5de9f3fc3cd21c79ccc149a5fe302200ea2f0
Martin Herold <you@example.com>
2020-11-21 11:14:40 +0100

    added models, template

---
+++ tweets/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+from django.http import HttpResponse, Http404, JsonResponse
+
+from .models import Tweet
+
+# Create your views here.
+
+
+def home_view(request, *args, **kwargs):
+    return HttpResponse('<h1>Hello World</h1>')
+
+
+def tweet_detail_view(request, tweet_id):
+    """
+    REST API VIEW
+    return json data
+    """
+    data = {
+        "id": tweet_id,
+    }
+    status = 200
+    try:
+        obj = Tweet.objects.get(id=tweet_id)
+        data['content'] = obj.content
+    except:
+        data['message'] = 'Not found'
+        status = 404
+
+    return JsonResponse(data, status=status)
